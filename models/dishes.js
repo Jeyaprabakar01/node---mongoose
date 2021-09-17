d5500de73c03fd9ffe6852491e3fa53c4cc37023
Jeyaprabakar01 <ej.prabakar@gmail.com>
2021-09-17 11:02:51 +0530

    Mogoose 2

--- models/dishes.js
+++ models/dishes.js
@@ -2,6 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+
+const commentSchema = new Schema({
+
+    rating: {
+
+        type: Number,
+        min: 1,
+        max:5,
+        required: true
+
+    },
+
+    comment :{
+
+        type: String,
+        required:true
+    },
+
+    author: {
+        type: String,
+        required : true
+
+    }
+    
+},{
+
+ timestamps: true
+    
+
+
+});
+
+
 const dishSchema = new Schema({
 
         name: {
@@ -17,7 +50,9 @@ const dishSchema = new Schema({
             type: String,
             required: true
 
-        }
+        },
+        comments: [commentSchema]
+
     },{
 
         timestamps: true
@@ -30,4 +65,3 @@ const dishSchema = new Schema({
 
     module.exports = Dishes;
 
-    
